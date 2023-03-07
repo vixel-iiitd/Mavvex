05cfe55861cac019def05ef7896319c1f5fa6ab1
vixel-iiitd <rahul19191@iiitd.ac.in>
2023-03-07 23:37:32 +0530

    created-react app

---
+++ mavvex/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+// import './components/nav.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <nav /> */}
+      {/* <p>Hi</p> */}
+      {/* The rest of your app goes here */}
+    </div>
+  );
+}
+
+export default App;
+
+
