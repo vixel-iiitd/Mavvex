e5bb421da5bc8dec971d97cc32694918554bb7d6
vixel-iiitd <rahul19191@iiitd.ac.in>
2023-03-12 23:21:48 +0530

    section-update

--- mavvex/src/components/UseCase1.jsx
+++ mavvex/src/components/UseCase1.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./UseCase1.css";
+import back1 from "../Images/back2.png";
 
 function UseCase1() {
   return (
-    <div className="container" id = 'UseCase1'>
-        <h>Deep Tech Product Development</h>
+    <div
+      className="container"
+      id="UseCase1"
+    >
+      <h>Deep Tech Product Development</h>
     </div>
   );
 }
